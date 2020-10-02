93f853ce410c196b5f1087b958624a41aeac217a
Teodor Grigor <grigor.teodor@gmail.com>
2020-10-02 18:05:58 +0100

    license added

--- material-elements/java/com/zeoflow/material/elements/button/MaterialButton.java
+++ material-elements/java/com/zeoflow/material/elements/button/MaterialButton.java
@@ -1,12 +1,12 @@
-/*
- * Copyright (C) 2020 ZeoFlow
- *
+/**
+ * Copyright 2020 ZeoFlow SRL
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -356,6 +356,7 @@ public class MaterialButton extends AppCompatButton implements Checkable, Shapea
    *
    * @hide
    */
+  @SuppressLint("RestrictedApi")
   @RestrictTo(LIBRARY_GROUP)
   @Override
   public void setSupportBackgroundTintMode(@Nullable PorterDuff.Mode tintMode)
