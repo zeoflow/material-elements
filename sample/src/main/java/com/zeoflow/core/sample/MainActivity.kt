ae7b3c796abb2b14c3f9441289a55894f18aa96d
Teodor Grigor <grigor.teodor@gmail.com>
2020-10-02 19:25:39 +0100

    updated demo

--- sample/src/main/java/com/zeoflow/core/sample/MainActivity.kt
+++ sample/src/main/java/com/zeoflow/core/sample/MainActivity.kt
@@ -9,11 +9,5 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
-    val mbl = findViewById<MaterialButtonLoading>(R.id.mbl)
-    mbl.setOnClickListener{
-      print(21)
-      mbl.setLoading(true)
-    }
   }
 }
