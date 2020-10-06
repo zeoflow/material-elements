c6b5a8403a24ffd66ee53f4e928ea47ad0091dca
Teodor Grigor <grigor.teodor@gmail.com>
2020-10-06 18:24:02 +0100

    improved autosize textview

---
+++ material-elements/java/com/zeoflow/material/elements/textview/AutosizeTextView.java
@@ -0,0 +1,24 @@
+package com.zeoflow.material.elements.textview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+
+public class AutosizeTextView extends MaterialTextView
+{
+  public AutosizeTextView(Context context)
+  {
+    super(context);
+  }
+
+  public AutosizeTextView(Context context, @Nullable AttributeSet attrs)
+  {
+    super(context, attrs);
+  }
+
+  public AutosizeTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+  {
+    super(context, attrs, defStyleAttr);
+  }
+}
