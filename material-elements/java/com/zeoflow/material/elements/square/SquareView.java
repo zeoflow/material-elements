1d6e8d92316698c59438df55157a6ca12d8778ca
Teodor Grigor <grigor.teodor@gmail.com>
2020-10-07 14:26:44 +0100

    finialized class import
    Co-Authored-By: Teogor <70129978+teogor@users.noreply.github.com>

--- material-elements/java/com/zeoflow/material/elements/square/SquareView.java
+++ material-elements/java/com/zeoflow/material/elements/square/SquareView.java
@@ -1,13 +1,33 @@
 package com.zeoflow.material.elements.square;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
-public class SquareView
+public class SquareView extends View
 {
 
-  public static void setSquare(View view)
+  public SquareView(Context context)
   {
+    super(context);
+  }
 
+  public SquareView(Context context, AttributeSet attrs)
+  {
+    super(context, attrs);
+  }
+
+  public SquareView(Context context, AttributeSet attrs, int defStyle)
+  {
+    super(context, attrs, defStyle);
+  }
+
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+  {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    int width = getMeasuredWidth();
+    setMeasuredDimension(width, width);
   }
 
 }
